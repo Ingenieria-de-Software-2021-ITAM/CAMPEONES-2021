f935f080a61d996c4ef1425dd0c01d75d6210d85
rorode8 <rorode8@hotmail.com>
2021-11-07 13:18:53 -0600

    voting feature is now working

--- web/flaskblog/models.py
+++ web/flaskblog/models.py
@@ -1,10 +1,16 @@
 from flaskblog import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+import pytz
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+likes = db.Table('likes',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +19,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    liked_posts = db.relationship('Post', secondary=likes,backref=db.backref('liked_by', lazy='dynamic'))
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
@@ -20,7 +27,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=lambda :datetime.now(pytz.timezone('Mexico/General')))
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
